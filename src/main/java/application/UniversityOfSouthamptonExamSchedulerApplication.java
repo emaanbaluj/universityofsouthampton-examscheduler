b151a2b769d06515a9de00859a1b76ecb7345b10
Emaan Baluj <emaanbaluj@icloud.com>
2024-08-02 11:02:18 +0100

    Created models for data encapsulation

---
+++ src/main/java/application/UniversityOfSouthamptonExamSchedulerApplication.java
@@ -0,0 +1,13 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityOfSouthamptonExamSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityOfSouthamptonExamSchedulerApplication.class, args);
+    }
+
+}
